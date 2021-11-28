12520499ce36a5dea84e8a283495f3be0f4f54d1
ncku_desktop <dushiun@gmail.com>
2021-11-28 22:27:56 +0800

    update 1128

---
+++ clearup_ram.m
@@ -0,0 +1,6 @@
+clear;
+rm = javax.swing.RepaintManager.currentManager([]);
+dim = rm.getDoubleBufferMaximumSize();
+rm.setDoubleBufferMaximumSize(java.awt.Dimension(0,0));  % clear
+rm.setDoubleBufferMaximumSize(dim);  %restore original dim
+java.lang.System.gc();  % garbage-collect
