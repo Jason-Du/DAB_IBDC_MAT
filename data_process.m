16b169097891712664a3676791de560173de4b9f
ncku_desktop <dushiun@gmail.com>
2021-12-13 09:11:42 +0800

    1213 update

--- data_process.m
+++ data_process.m
@@ -1,10 +1,18 @@
 % clear;
-% matfile='./DATASET/MOSFET/MOSFET/Test_26_run_1.mat';
+% matfile='./DATASET/MOSFET/Test_26_run_1.mat';
 % data = load(matfile,'-mat');
-data_numbers=39223;
-Vds_array= zeros(data_numbers,500);
-ID_array= zeros(data_numbers,500);
-for i= 1: 39223
-    Vds_array(i,:)=data.measurement.transient(i).timeDomain.drainSourceVoltage;
-    ID_array(i,:)=data.measurement.transient(i).timeDomain.drainCurrent;
-end
+
+% data_numbers=39223;% 39223秒
+% sample_rate=2e-6;
+% transient_sample_scale=500;
+% Vds_array= zeros(data_numbers,transient_sample_scale);
+% ID_array= zeros(data_numbers,transient_sample_scale);
+% time=zeros(data_numbers*transient_sample_scale,1);
+% for i= 1: 39223
+%     Vds_array(i,:)=data.measurement.transient(i).timeDomain.drainSourceVoltage;
+%     ID_array(i,:)=data.measurement.transient(i).timeDomain.drainCurrent;
+% end
+
+test=data.measurement.transient(1).date;
+a=convertCharsToStrings(test);
+b=split(a,[" ",":"]);
